365edf73061b321db45f87663579d05820f124f9
Scott Dawson <scott@uu.net.nz>
2023-12-20 12:45:00 +1100

    Get remote address behind proxy

--- internal/web/request.go
+++ internal/web/request.go
@@ -1,41 +1,51 @@
 package web
 
 import (
-    "bytes"
-    "encoding/json"
-    "io"
-    "net/http"
-    "strings"
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
 )
 
 // CopyBody creates a copy of the request body without modifying the original
 func CopyBody(request *http.Request) io.ReadCloser {
-    buffer, _ := io.ReadAll(request.Body)
-    request.Body = io.NopCloser(bytes.NewBuffer(buffer))
+	buffer, _ := io.ReadAll(request.Body)
+	request.Body = io.NopCloser(bytes.NewBuffer(buffer))
 
-    return io.NopCloser(bytes.NewBuffer(buffer))
+	return io.NopCloser(bytes.NewBuffer(buffer))
 }
 
 // DecodeBody decodes the request body into a struct and fixes common issues
 func DecodeBody(request *http.Request, into any) error {
-    err := json.NewDecoder(CopyBody(request)).Decode(&into)
-    if err == nil {
-        return nil
-    }
+	err := json.NewDecoder(CopyBody(request)).Decode(&into)
+	if err == nil {
+		return nil
+	}
 
-    // Attempt to fix
-    buffer, _ := io.ReadAll(CopyBody(request))
-    strBuffer := string(buffer)
+	// Attempt to fix
+	buffer, _ := io.ReadAll(CopyBody(request))
+	strBuffer := string(buffer)
 
-    // Invalid mac address list
-    if !strings.Contains(strBuffer, `"mac_address_list":["`) {
-        strBuffer = strings.Replace(strBuffer, `"mac_address_list":[`, `"mac_address_list":["`, 1)
-        request.Body = io.NopCloser(bytes.NewBuffer([]byte(strBuffer)))
-        err = json.NewDecoder(CopyBody(request)).Decode(&into)
-        if err != nil {
-            return err
-        }
-    }
+	// Invalid mac address list
+	if !strings.Contains(strBuffer, `"mac_address_list":["`) {
+		strBuffer = strings.Replace(strBuffer, `"mac_address_list":[`, `"mac_address_list":["`, 1)
+		request.Body = io.NopCloser(bytes.NewBuffer([]byte(strBuffer)))
+		err = json.NewDecoder(CopyBody(request)).Decode(&into)
+		if err != nil {
+			return err
+		}
+	}
 
-    return nil
+	return nil
+}
+
+// getRemoteAddress will get the originating IP(s)
+func getRemoteAddress(request *http.Request) string {
+	forwardedFor := request.Header.Get("X-Forwarded-For")
+	if forwardedFor != "" {
+		return forwardedFor
+	}
+
+	return request.RemoteAddr
 }
