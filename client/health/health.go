e1c8ac6a014fa7f299311b9e3a894245b7d9644e
Scott Dawson <scott@uu.net.nz>
2023-12-22 08:52:56 +1100

    Don't debug log healthchecks

--- client/health/health.go
+++ client/health/health.go
@@ -1,12 +1,17 @@
 package health
 
 import (
-    "net/http"
-
-    "github.com/sjdaws/dls/internal/web"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 // GetHealth returns "OK" if the service is up
 func GetHealth(response http.ResponseWriter, request *http.Request) {
-    web.Respond(request, response, "text/plain", "OK")
+	body := "OK"
+
+	// Write response directly to bypass debug logging
+	response.Header().Add("Content-Length", fmt.Sprintf("%d", len(body)))
+	response.WriteHeader(http.StatusOK)
+	_, _ = io.WriteString(response, body)
 }
